d77887e19439f164b0ce0a75c0aa38af92fcd3b7
Fernando Spizzca <fernando.spizzca@gmail.com>
2026-02-01 19:44:23 -0300

    Second Commit

--- kafka-basics/src/main/java/br/spizzca/demos/kafka/ProducerDemoKeys.java
+++ kafka-basics/src/main/java/br/spizzca/demos/kafka/ProducerDemoKeys.java
@@ -34,13 +34,17 @@ public class ProducerDemoKeys {
         properties.put("bootstrap.servers", "kafka-227b8376-fernando-spizzca.c.aivencloud.com:27517");
         properties.put("security.protocol", "SSL");
         properties.put("ssl.protocol", "TLS");
+        // Opcional: senha específica da chave se for diferente da do keystore
+        properties.put("ssl.key.password", "AVNS_QT5n5bcsLRmCv206GLP");
 
         // Keystore
         properties.put("ssl.keystore.location", "D:\\OneDrive\\Documentos\\Cursos\\Kafka\\certificados_aiven\\client.keystore.p12");
+        properties.put("ssl.keystore.password", "AVNS_QT5n5bcsLRmCv206GLP");
         properties.put("ssl.keystore.type", "PKCS12");
 
         // Truststore
         properties.put("ssl.truststore.location", "D:\\OneDrive\\Documentos\\Cursos\\Kafka\\certificados_aiven\\client.truststore.jks");
+        properties.put("ssl.truststore.password", "AVNS_QT5n5bcsLRmCv206GLP");
         properties.put("ssl.truststore.type", "JKS");
 
         // set producer properties
